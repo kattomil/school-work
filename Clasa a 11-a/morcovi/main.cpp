1808dbbc499cda65186e713dcce8196d6d9f5546
kattomil <tobyytb7@gmail.com>
2020-02-18 16:19:50 +0200

    morcovete

---
+++ Clasa a 11-a/morcovi/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int n,m,a[100][100];
+int main() {
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>a[i][j];
+    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) a[i][j]+=a[i-1][j]+a[i][j-1];
+    cout<<endl;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=m;j++) cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+    cout<<endl<<a[n][m]<<" morcovi pana la margine."<<endl;
+    return 0;
+}
